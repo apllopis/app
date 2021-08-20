3c278106d6b679632242c0d69a5ad87d36b85eac
apllopis <alberto.pintadollopis@gmail.com>
2021-08-20 20:13:27 +0200

    terminando #03 😅

--- src/index.js
+++ src/index.js
@@ -2,33 +2,69 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { useState } from "react";
+
+const ListofClicks = ({ clicks }) => {
+  return (
+    <>
+      <p>Clicks totales: {clicks.length}</p>
+      {clicks.join(", ")}
+    </>
+  );
+};
+const Nousado = () => {
+  return <h1>No usado todavía</h1>;
+};
+
+const Counter = ({ contador }) => {
+  return <h2>{contador}</h2>;
+};
+
 const rootElement = document.getElementById("root");
 
-const App = ({ inicial }) => {
-  const [contador, setContador] = useState(inicial);
+const App = () => {
+  const [contador, setContador] = useState({
+    izda: 0,
+    dcha: 0,
+  });
+  const [clicks, setClicks] = useState([]);
 
-  const handleClickAdd = () => {
-    setContador(contador + 1);
+  const handleClickLeft = () => {
+    const nuevoEstadoContador = {
+      ...contador,
+      izda: contador.izda + 1,
+    };
+    setContador(nuevoEstadoContador);
+    setClicks((prevClicks) => [...prevClicks, "U"]);
   };
+
   const handleClickReset = () => {
-    setContador(inicial);
+    const nuevoEstadoContador = {
+      ...contador,
+      izda: 0,
+      dcha: 0,
+    };
+    setContador(nuevoEstadoContador);
+    setClicks((prevClicks) => [...prevClicks, "Rst"]);
   };
-  const handleClickDec = () => {
-    setContador(contador - 1);
+  const handleClickRight = () => {
+    const nuevoEstadoContador = {
+      ...contador,
+      dcha: contador.dcha + 1,
+    };
+    setContador(nuevoEstadoContador);
+    setClicks((prevClicks) => [...prevClicks, "D"]);
   };
-
-  const isEven = contador % 2 === 0;
-  const mensaje = isEven ? "Par" : "Impar";
 
   return (
     <div>
       <h1> Clase3</h1>
-      <h2>{contador}</h2>
-      <p>{mensaje} </p>
-      <button onClick={handleClickDec}>-</button>
+      <Counter contador={contador.izda} />
+      <button onClick={handleClickLeft}>Superior</button>
       <button onClick={handleClickReset}>Reset</button>
-      <button onClick={handleClickAdd}>+</button>
+      <button onClick={handleClickRight}>Inferior</button>
+      <Counter contador={contador.dcha} />
+      {clicks.length === 0 ? <Nousado /> : <ListofClicks clicks={clicks} />}
     </div>
   );
 };
-ReactDOM.render(<App inicial={0} />, rootElement);
+ReactDOM.render(<App />, rootElement);
