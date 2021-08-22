615c5ab53cd213a300c3d9c9a2ae636db6527749
apllopis <alberto.pintadollopis@gmail.com>
2021-08-22 14:12:35 +0200

    formularios clase4 🍟

--- src/App.js
+++ src/App.js
@@ -4,32 +4,41 @@
  * se tiene que importar como :
  *  import {Note} from "./components/Note.js"
  */
+import { useState } from "react";
 import "./App.css";
 import Note from "./components/Note";
-const notes = [
-  {
-    id: 12,
-    content: "Cambiamos aros",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 13,
-    content: "revision cables bogies",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-    categories: ["electricidad"],
-  },
-  {
-    id: 14,
-    content: "Engrase completo",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-    categories: ["mantenimiento", "revisión"],
-  },
-];
 
-function App() {
+/**  Se pasa una props a App y si no le llega
+ * nada se crea como un array vacío
+ */
+function App({ notes = [] }) {
+  const [notas, setNotas] = useState(notes);
+  const [nuevaNota, setNuevaNota] = useState("");
+  const [showAll, setShowAll] = useState(true);
+  const handleChange = (event) => {
+    setNuevaNota(event.target.value);
+  };
+
+  const handleShowAll = () => {
+    setShowAll(() => !showAll);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const notaToAddToState = {
+      id: notas.length + 1,
+      content: nuevaNota,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.55, // para que varíe
+      categories: ["mantenimiento"],
+    };
+    /**  [notas.concat(notaToAddToState)]
+     *  version con concat
+     */
+
+    setNotas([...notas, notaToAddToState]);
+    setNuevaNota("");
+  };
+
   /**
    * la key en el elemento mas alto en este caso
    * en el elemnto <Note />
@@ -39,20 +48,35 @@ function App() {
    * por
    * {notes.map(note=> (.....))}
    *  si solo está el  return  en la función
+   *
+   * Poner en el campo input value para que react controle el imput
+   * en lugar del DOM
    */
   return (
     <div>
-      <h1>parte4</h1>
-      <ul>
-        {notes.map((note) => (
-          <Note
-            categories={note.categories}
-            content={note.content}
-            date={note.date}
-            key={note.id}
-          />
-        ))}
-      </ul>
+      <h1>Mantenimientos</h1>
+      <button onClick={handleShowAll}>
+        {showAll ? "Sólo las notas importantes" : "Todas las notas"}
+      </button>
+      <ol>
+        {notas
+          .filter((note) => {
+            if (showAll === true) return true;
+            return note.important === true;
+          })
+          .map((note) => (
+            <Note
+              categories={note.categories}
+              content={note.content}
+              date={note.date}
+              key={note.id}
+            />
+          ))}
+      </ol>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={nuevaNota} />
+        <button>Grabar mantenimiento</button>
+      </form>
     </div>
   );
 }
